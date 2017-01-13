78465c0fabda5da729b781cf72447ed5098d2a5e
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-01-13 11:43:50 +0100

    init plugin functional test

---
+++ test/groovy/org/m2ci/msp/praat/PraatPluginTest.groovy
@@ -0,0 +1,29 @@
+package org.m2ci.msp.praat
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.*
+
+import static org.gradle.testkit.runner.TaskOutcome.*
+
+class PraatPluginTest {
+
+    File projectDir
+    File buildScriptFile
+    GradleRunner gradle
+
+    @BeforeClass
+    void setUp() {
+        projectDir = File.createTempDir()
+        def buildScript = this.getClass().getResourceAsStream('build.gradle').text
+        assert buildScript
+        buildScriptFile = new File(projectDir, 'build.gradle')
+        buildScriptFile.text = buildScript
+        gradle = GradleRunner.create().withProjectDir(projectDir).withPluginClasspath()
+    }
+
+    @Test
+    void testConfigure() {
+        def result = gradle.build()
+        assert result.task(':help').outcome == SUCCESS
+    }
+}
