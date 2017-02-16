f8b552d98d3740d1d4067e10f72e8e79567cada7
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-02-16 14:40:06 +0100

    extract praat in custom task
    also use same resource path for each os

---
+++ src/main/groovy/org/m2ci/msp/praat/ExtractPraat.groovy
@@ -0,0 +1,21 @@
+package org.m2ci.msp.praat
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.*
+
+class ExtractPraat extends DefaultTask {
+
+    @OutputFile
+    File binary
+
+    ExtractPraat() {
+        binary = binary ?: project.file("$temporaryDir/praat")
+    }
+
+    @TaskAction
+    void extract() {
+        binary.withOutputStream { stream ->
+            stream << this.getClass().getResourceAsStream('/org/praat/praat')
+        }
+    }
+}
--- src/main/groovy/org/m2ci/msp/praat/PraatWrapperPlugin.groovy
+++ src/main/groovy/org/m2ci/msp/praat/PraatWrapperPlugin.groovy
@@ -4,5 +4,6 @@ import org.gradle.api.*
 
 class PraatWrapperPlugin implements Plugin<Project> {
     void apply(Project project) {
+        project.task('praat', type: ExtractPraat)
     }
 }
