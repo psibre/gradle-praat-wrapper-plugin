4bfc2e61d30af1cd9b66d1f8cf735f2ba1ebd041
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-02-17 17:03:25 +0100

    test praat version

--- src/test/groovy/org/m2ci/msp/praat/PraatWrapperPluginTest.groovy
+++ src/test/groovy/org/m2ci/msp/praat/PraatWrapperPluginTest.groovy
@@ -38,9 +38,12 @@ class PraatWrapperPluginTest {
 
     @Test
     void testCanRunPraat() {
-        def result = gradle.withArguments('canRunPraat').build()
+        def result = gradle.withArguments('--quiet', 'canRunPraat').build()
         println result.output
         assert result.task(':canRunPraat').outcome == SUCCESS
+        def expected = this.getClass().getResourceAsStream('/org/praat/version.txt').text.trim()
+        def actual = result.output.trim()
+        assert actual == expected
     }
 
 }
