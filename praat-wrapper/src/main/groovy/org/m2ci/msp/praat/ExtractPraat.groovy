282b36659fa6f310142c1ef7d1b0497dd09255ef
Ingmar Steiner <steiner@coli.uni-saarland.de>
2017-03-03 17:53:27 +0100

    use OS-specific binary resource names

--- praat-wrapper/src/main/groovy/org/m2ci/msp/praat/ExtractPraat.groovy
+++ praat-wrapper/src/main/groovy/org/m2ci/msp/praat/ExtractPraat.groovy
@@ -13,14 +13,28 @@ class ExtractPraat extends DefaultTask {
     String version = this.getClass().getResourceAsStream('/org/praat/version.txt').text.trim()
 
     ExtractPraat() {
-        def binaryName = OperatingSystem.current().isWindows() ? 'praat.exe' : 'praat'
+        def binaryName = 'praat'
+        switch (OperatingSystem.current()) {
+            case { it.isMacOsX() }:
+                binaryName += '-mac'
+                break
+            case { it.isLinux() }:
+                binaryName += '-linux'
+                break
+            case { it.isWindows() }:
+                // detect architecture:
+                binaryName = System.getenv("ProgramFiles(x86)") ? 'praat-windows64.exe' : 'praat-windows.exe'
+                break
+            default:
+                break
+        }
         binary = binary ?: project.file("$temporaryDir/$binaryName")
     }
 
     @TaskAction
     void extract() {
         binary.withOutputStream { stream ->
-            stream << this.getClass().getResourceAsStream('/org/praat/praat')
+            stream << this.getClass().getResourceAsStream("/org/praat/$binary.name")
         }
         binary.executable = true
     }
