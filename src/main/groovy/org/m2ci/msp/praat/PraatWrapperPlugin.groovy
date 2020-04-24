d9c54cb7e7ee6b1410c822eb660ac61f3ef54b29
Ingmar Steiner <isteiner@audeering.com>
2020-04-24 06:26:00 +0200

    Use exclusive content filtering for custom repository

--- src/main/groovy/org/m2ci/msp/praat/PraatWrapperPlugin.groovy
+++ src/main/groovy/org/m2ci/msp/praat/PraatWrapperPlugin.groovy
@@ -1,6 +1,5 @@
 package org.m2ci.msp.praat
 
-
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.Copy
@@ -17,13 +16,20 @@ class PraatWrapperPlugin implements Plugin<Project> {
 
         project.repositories {
             def praatVersionNoDots = praatVersion.replaceAll(~/\./, '')
-            ivy {
-                url 'http://www.fon.hum.uva.nl/praat/old/'
-                patternLayout {
-                    artifact "$praatVersionNoDots/[module]${praatVersionNoDots}_[classifier].[ext]"
+            exclusiveContent {
+                forRepository {
+                    ivy {
+                        url 'http://www.fon.hum.uva.nl/praat/old/'
+                        patternLayout {
+                            artifact "$praatVersionNoDots/[module]${praatVersionNoDots}_[classifier].[ext]"
+                        }
+                        metadataSources {
+                            artifact()
+                        }
+                    }
                 }
-                metadataSources {
-                    artifact()
+                filter {
+                    includeGroup "org.praat"
                 }
             }
         }
