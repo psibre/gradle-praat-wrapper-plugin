9d0750806be747f581439be2f52795e5c540c4e7
Ingmar Steiner <isteiner@audeering.com>
2020-04-25 08:38:39 +0200

    Test oldest compatible Gradle version

--- src/test/groovy/org/m2ci/msp/praat/PraatWrapperLegacyGradleTest.groovy
+++ src/test/groovy/org/m2ci/msp/praat/PraatWrapperLegacyGradleTest.groovy
@@ -16,4 +16,16 @@ class PraatWrapperLegacyGradleTest {
         def gradle = GradleRunner.create().withGradleVersion('6.1.1').withProjectDir(projectDir).withPluginClasspath()
         gradle.buildAndFail()
     }
+
+    @Test
+    void 'Gradle v6.2 can apply plugin'() {
+        def projectDir = File.createTempDir()
+        new File(projectDir, 'build.gradle').withWriter {
+            def resourceStream = this.class.getResourceAsStream('build.gradle')
+            assert resourceStream
+            it << resourceStream
+        }
+        def gradle = GradleRunner.create().withGradleVersion('6.2').withProjectDir(projectDir).withPluginClasspath()
+        gradle.build()
+    }
 }
