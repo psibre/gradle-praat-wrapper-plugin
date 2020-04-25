0feff3dda02384c5b4230188716976df3a6efd0a
Ingmar Steiner <isteiner@audeering.com>
2020-04-25 08:33:06 +0200

    Test newest incompatible Gradle version

---
+++ src/test/groovy/org/m2ci/msp/praat/PraatWrapperLegacyGradleTest.groovy
@@ -0,0 +1,19 @@
+package org.m2ci.msp.praat
+
+import org.gradle.testkit.runner.GradleRunner
+import org.testng.annotations.Test
+
+class PraatWrapperLegacyGradleTest {
+
+    @Test
+    void 'Gradle v6.1.1 cannot apply plugin'() {
+        def projectDir = File.createTempDir()
+        new File(projectDir, 'build.gradle').withWriter {
+            def resourceStream = this.class.getResourceAsStream('build.gradle')
+            assert resourceStream
+            it << resourceStream
+        }
+        def gradle = GradleRunner.create().withGradleVersion('6.1.1').withProjectDir(projectDir).withPluginClasspath()
+        gradle.buildAndFail()
+    }
+}
